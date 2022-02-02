505f9ca7dd889b505efd8edbe6a99622d9efd5e3
Your Name <victor_estevez@ucne.edu.do>
2022-02-02 09:23:35 -0400

    Creación de UI y archivos

---
+++ UI/Consultas/cEjemplo.xaml.cs
@@ -0,0 +1,23 @@
+using System.Windows;
+using Victor_Estevez_Ap1_p1.Entidades;
+using Ejemplo.BLL;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Victor_Estevez_Ap1_p1.UI.Consultas
+{
+    public partial class cEjemplo : Window
+    {
+
+        public cEjemplo()
+        {
+            InitializeComponent();
+        }
+
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ UI/Registros/rEjemplo.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows;
+using Victor_Estevez_Ap1_p1.Entidades;
+using Victor_Estevez_Ap1_p1.BLL;
+
+namespace Victor_Estevez_Ap1_p1.UI.Registros
+{
+    public partial class rEjemplo : Window
+    {
+        
+    }
+}
